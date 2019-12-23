95327371736eb379c61317edc19bf46b7e525b3f
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2019-12-23 13:31:18 +0300

    Test guarded version as well

---
+++ bug890_with_main_guard.py
@@ -0,0 +1,12 @@
+import multiprocessing
+import os
+
+if __name__ == "__main__":
+
+    if not os.path.exists("./tmp"): os.mkdir("./tmp")
+    os.chdir("./tmp")
+
+    with multiprocessing.Manager() as manager:
+        pass
+
+    print("ok")
